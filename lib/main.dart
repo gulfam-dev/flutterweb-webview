b7419f518770e0dc37b820b2f97a96e6c3730df2
Gulfam <gulfam.dev@gmail.com>
2024-01-26 23:48:49 +0500

    first update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -41,7 +44,7 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFFE5EFF1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -65,17 +68,65 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  late bool isOnline; // Variable to track internet connectivity status
+
+  @override
+  void initState() {
+    super.initState();
+    checkConnectivity(); // Check connectivity when the widget is initialized
+    Connectivity().onConnectivityChanged.listen((result) {
+      // Listen for changes in connectivity status
+      setState(() {
+        isOnline = result != ConnectivityResult.none;
+      });
+    });
+
+  }
+
+
+  Future<void> requestCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (status.isDenied) {
+      await Permission.camera.request();
+    }
+  }
+
+
+
+  Future<void> checkConnectivity() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    setState(() {
+      isOnline = connectivityResult != ConnectivityResult.none;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: WillPopScope(
         onWillPop: showExitDialog,
-        child: const SafeArea(
-          child: WebView(
-            initialUrl: 'https://www.glamastay.co.uk/',
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
+        child: SafeArea(
+          child: isOnline
+              ? const WebView(
+                  initialUrl: 'https://www.glamastay.co.uk/',
+                  javascriptMode: JavascriptMode.unrestricted,
+                )
+              : Center(
+                  child: Padding(
+                  padding: const EdgeInsets.only(bottom: 200),
+                  child: Container(child: Image.asset('images/nointernet.png')),
+                )),
         ),
+      ),
+    );
+  }
+
+  Widget loading() {
+    return const Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.green),
       ),
     );
   }
